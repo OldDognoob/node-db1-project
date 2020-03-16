e874a03bf66c2af365e6ed922961a1607d6794f4
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-16 16:19:22 +0200

    helpers-file

--- accounts/account-model.js
+++ accounts/account-model.js
@@ -1,2 +1,37 @@
 //import database config
-const db = require('../data/dbConfig');
+const db = require("../data/dbConfig");
+
+//DB helpers
+function getAllAccounts() {
+  return db("accounts");
+}
+
+function getAccountById(id) {
+  return db("account")
+    .where({ id })
+    .select("id", "account", "budget");
+}
+
+function insertNewAccount({ name, budget }) {
+  return db("accounts").insert({ name, budget });
+}
+
+function updateAccountById({ id, name, budget }) {
+  return db("accounts")
+    .where({ id })
+    .update({ name, budget });
+}
+
+function deleteAccountById(id) {
+  return db("accounts")
+    .where({ id })
+    .delete();
+}
+
+module.exports = {
+  getAllAccounts,
+  getAccountById,
+  insertNewAccount,
+  updateAccountById,
+  deleteAccountById
+};
--- queries.sql
+++ queries.sql
@@ -5,14 +5,14 @@ select * from customers where postalcode = '1010';
 -- Find the phone number for the supplier with the id 11
 select Phone from Suppliers where SupplierID = 11;
 -- List first 10 orders placed, sorted descending by the order date
-select * from Orders ORDER BY OrderDate DESC limit 10;
+SELECT * from Orders ORDER BY OrderDate desc limit "10";
 -- Find all customers that live in London, Madrid, or Brazil
 select * from Customers where City = 'Madrid' OR City = 'London' OR Country = 'Brazil';
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 insert into Customers (CustomerName, ContactName, Address, City, PostalCode, Country) VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
-update Customers set PostalCode = '11122' where CustomerID = 92;   
+update Customers set PostalCode = '11122' where CustomerID = 93;   
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 select distinct City from Customers;
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
-select SupplierName from suppliers where length(SupplierName) > 20;
+select * from suppliers where length(SupplierName) > 20
